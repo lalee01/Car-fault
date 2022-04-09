6fea1d450a4f1bdeccfd7758b6bbe53469572029
lalee01 <sciro920119@gmail.com>
2022-04-09 12:08:06 +0200

    chore: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Homepage from './Component/Homepage/Homepage'
-import Login from './Component/Login/Login.tsx'
 import Auth from './Component/Auth/Auth.js'
 
 const App = () => <Auth/>
